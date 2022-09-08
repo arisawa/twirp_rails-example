5a73b1310a39ad86f7287f4138885cb7ed6b366f
Kosuke Arisawa <arisawa@gmail.com>
2022-09-08 20:14:16 +0900

    Add routes for RPC

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get '/hello' => HelloController.action(:hello)
+
+  namespace :twirp do
+    post 'example.v1.UserApi/ListUsers' => UsersController.action(:list_users)
+    post 'example.v1.UserApi/GetUser' => UsersController.action(:get_user)
+  end
 end
