b69c228f64e82a7b3ce95bfcd43579c7e8947045
Kosuke Arisawa <arisawa@gmail.com>
2022-09-08 11:40:10 +0900

    Add endpoints for User RPC

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/list_users' => UsersController.action(:list_users)
+  post '/get_user' => UsersController.action(:get_user)
 end
